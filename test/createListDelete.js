b132f2559aece335ecbe44e6d33d373e677c2eef
Pete Johnson <petecj2@gmail.com>
2016-12-19 15:29:24 -0800

    Changed file name and name of test

--- test/createListDelete.js
+++ test/createListDelete.js
@@ -2,7 +2,8 @@ var assert = require('assert');
 var request = require('request');
 var fs = require('fs');
 
-describe('Create, Read, Delete', function() {
+describe('Create, List, Delete', function() {
+	this.timeout(5000);
     it('should create a new ToDo, read it, & delete it', function(done) {
 		// Build and log the path
 		var path = "https://" + process.env.TODOS_ENDPOINT + "/dev/todos";
