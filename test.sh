b7e5c3746d3b81a2940814531dcb0f270c67b34e
Pete Johnson <petecj2@gmail.com>
2017-01-05 14:26:19 -0800

    Moved deploy and test execution back to same phase, added text to env var setting

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 # Assuming you deployed with a 'serverless deploy | tee deploy.out' this script will auto fill in the TODOS_ENDPOINT environment variable and execute your tests
 node getEndpoint.js > endpoint.out
 value=`cat endpoint.out`
-echo $value
+echo "Here's the endpoint: " $value
 export TODOS_ENDPOINT=$value
 npm test
