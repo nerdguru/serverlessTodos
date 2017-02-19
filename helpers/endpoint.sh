3a92252e208322f97345d9dfd36e3c32c4582b0d
sbstjn <mail@sbstjn.com>
2017-02-19 22:01:37 +0100

    Update test scripts

--- helpers/endpoint.sh
+++ helpers/endpoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DIR_BUILD=$DIR/../.build
-
 # Assuming you deployed with a 'serverless deploy | tee deploy.out' this script
 # will auto fill in the TODOS_ENDPOINT environment variable
 
-node $DIR/getEndpoint.js > $DIR_BUILD/endpoint.out
-value=`cat $DIR_BUILD/endpoint.out`
-export TODOS_ENDPOINT=$value
+node helpers/getEndpoint.js > .build/endpoint.out
+export TODOS_ENDPOINT=`cat .build/endpoint.out`
 
 echo $TODOS_ENDPOINT
